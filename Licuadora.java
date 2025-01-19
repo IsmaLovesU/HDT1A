a7d6de0696741cf40f86df16ec266b93d98a6611
Marcelo Detlefsen <83415711+MarceloDetlefsen@users.noreply.github.com>
2025-01-18 19:42:43 -0600

    Se hacen ls Overrides a los metodos que utilizará la licuadora

--- Licuadora.java
+++ Licuadora.java
@@ -3,47 +3,112 @@ public class Licuadora implements Interfaz
     private boolean encendido;
     private boolean llenado;
     private int velocidad;
+    private static final int MAX_VELOCIDAD = 10;
 
     public Licuadora()
     {
-        encendido = false;
-        llenado = false;
-        velocidad = 0;
+        this.encendido = false;
+        this.llenado = false;
+        this.velocidad = 0;
     }
 
     @Override
     public void encender()
     {
-        encendido = true;
+        if (!encendido) 
+        {
+            encendido = true;
+            System.out.println("Licuadora encendida.");
+        } 
+        else 
+        {
+            System.out.println("La licuadora ya está encendida.");
+        }
     }
 
     @Override
     public void llenar()
     {
-        llenado = true;
+        if (!llenado) 
+        {
+            llenado = true;
+            System.out.println("Licuadora llena.");
+        } 
+        else 
+        {
+            System.out.println("La licuadora ya está llena.");
+        }
     }
 
     @Override
     public void aumentarVelocidad()
     {
-        velocidad++;
+        if (!encendido) 
+        {
+            System.out.println("No se puede aumentar la velocidad. La licuadora está apagada.");
+            return;
+        }
+
+        if (!llenado) 
+        {
+            System.out.println("No se puede aumentar la velocidad. La licuadora está vacía.");
+            return;
+        }
+
+        if (velocidad < MAX_VELOCIDAD) 
+        {
+            velocidad++;
+            System.out.println("Velocidad aumentada a: " + velocidad);
+        } 
+        else 
+        {
+            System.out.println("La licuadora ya está en la velocidad máxima.");
+        }
     }
 
     @Override
     public int consultarVelocidad()
     {
+        System.out.println("La velocidad actual es: " + velocidad);
         return velocidad;
     }
 
     @Override
     public boolean consultarLlenado()
     {
+        System.out.println(llenado ? "La licuadora está llena." : "La licuadora está vacía.");
         return llenado;
     }
 
     @Override
     public void vaciar()
     {
-        llenado = false;
-    }   
+        if (llenado) 
+        {
+            llenado = false;
+            System.out.println("Licuadora vaciada.");
+        } 
+        else 
+        {
+            System.out.println("La licuadora ya está vacía.");
+        }
+    }
+    
+    
+    public void apagar() 
+    {
+        if (encendido) 
+        {
+            encendido = false;
+            velocidad = 0; // Reiniciar velocidad al apagar
+            System.out.println("Licuadora apagada.");
+        } else {
+            System.out.println("La licuadora ya está apagada.");
+        }
+    }
+
+    public boolean isEncendido() 
+    {
+        return encendido;
+    }
 }
